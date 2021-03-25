213384be2af0ac4c73fbf684d9de7af8d7f2d99c
Juan Garcia <garcia.d.juan14@gmail.com>
2021-03-25 16:18:40 -0500

    loops

--- js/while.js
+++ js/while.js
@@ -21,29 +21,5 @@ do {
     } else if(conesSold > allCones) {
         console.log("i cannot sell you " + conesSold + " cones!")
     }
-} while (allCones > 0){
-    console.log("Yay! I sold them all");
-}
-
-
-
-
-
-
-
-// var priceOfItem = parseFloat(prompt("Enter the price of your first item"));
-// alert("The price of your item was " + priceOfItem);
-// // create accumulator variable
-// var totalCost = priceOfItem;
-// var userInput = "";
-// // create a sentinel value
-// while(userInput !== "STOP") {
-//     userInput = prompt("Enter the price of your next item \r\n\ Enter STOP when you are done");
-//     if (userInput === "STOP"){
-//         alert("OK. Your final total is " + totalCost);
-//     } else {
-//         priceOfItem = parseFloat(userInput);
-//         totalCost = totalCost + priceOfItem;
-//         alert("Your total is now " + totalCost.toFixed(2));
-//     }
-// }
+} while (allCones > 0);
+console.log("Yay! I sold them all");
