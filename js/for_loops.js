8e258ae16247bcc84ff54d19f1f4b42e1168811f
Juan Garcia <garcia.d.juan14@gmail.com>
2021-03-29 11:16:17 -0500

    loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,53 @@
 "use strict";
 
 
+//
+// function showMultiplicationTable(number) {
+//     for (var number =1 ; number <= 10; number++) {
+//         console.log("7 * " + number + " = " + (7 * number))
+//     }
+// }
+// showMultiplicationTable()
 
-function showMultiplicationTable(unit) {
-}
-for (var unit =1 ; unit <= 10; unit++) {
-    console.log("7 * " + unit + " = " + (7 * unit))
-}
+// for (var unit =1 ; unit <= 10; unit++) {
+//     console.log("7 * " + unit + " = " + (7 * unit))
+// }
+
+
+// function random (min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return math
+
+//
+// for (var unit = 1; unit <= 10; unit++)
+// {
+//     var random = Math.floor(Math.random() * 181) + 20;
+//     if (random % 2 === 0) {
+//         console.log(random + " is even")
+//     } else {
+//         console.log(random + " is odd")
+//     }
+// }
+//
+// function randomInterval (min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+//
+// var random, evenOddString;
+// for (var unit = 1; unit <= 10; unit++) {
+//     random = randomInterval(20, 200);
+//     evenOddString = (random % 2 === 0) 'even'
+// }
+
+//
+//
+// var output = ""
+//
+// for(var i = 1; i < 10; i++) {
+//     for(var j = 1; j <= i; j++){
+//         output += i + "";
+//     }
+//     console.log(output);
+//     output = "";
+// }
