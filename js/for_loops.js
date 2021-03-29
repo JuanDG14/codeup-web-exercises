2203c5ac66a667c5a611d5d054d9b39a0550ebb6
Juan Garcia <garcia.d.juan14@gmail.com>
2021-03-29 09:35:35 -0500

    for loops

---
+++ js/for_loops.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+
+function showMultiplicationTable(unit) {
+}
+for (var unit =1 ; unit <= 10; unit++) {
+    console.log(7 * unit)
+}
--- js/while.js
+++ js/while.js
@@ -8,18 +8,25 @@
 //     increase = increase * 2;
 // }
 
-
-var allCones = Math.floor(Math.random() * 50) + 50;
-
-do {
-    var conesSold = Math.floor(Math.random() * 5) + 1;
-    console.log("i have a total of " + allCones + " cones")
-    if (conesSold <= allCones) {
-        allCones = allCones - conesSold;
-        console.log("i sold " + conesSold + " cones");
-        console.log(allCones + " cones is all i have left");
-    } else if(conesSold > allCones) {
-        console.log("i cannot sell you " + conesSold + " cones!")
-    }
-} while (allCones > 0);
-console.log("Yay! I sold them all");
+// For loop
+// var count
+// for (count = 2; count <= 65536; count = count * 2){
+//     console.log(count)
+// }
+//
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// do {
+//     var conesSold = Math.floor(Math.random() * 5) + 1;
+//     console.log("i have a total of " + allCones + " cones")
+//     if (conesSold <= allCones) {
+//         allCones = allCones - conesSold;
+//         console.log("i sold " + conesSold + " cones");
+//         console.log(allCones + " cones is all i have left");
+//     } else if(conesSold > allCones) {
+//         console.log("i cannot sell you " + conesSold + " cones!")
+//     }
+// } while (allCones > 0);
+// console.log("Yay! I sold them all");
+//
+//
