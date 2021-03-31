7b506c32bd7af8c409af6d3b2fce3051c5fb48ef
Juan Garcia <garcia.d.juan14@gmail.com>
2021-03-30 22:33:22 -0500

    objects

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,33 @@
 "use strict";
 
 
-//
+
+var count = 0;
+var userNum = parseInt(prompt("Pick a odd number between 1 & 50"));
+while (userNum % 2 === 0 || isNaN(userNum) === true || userNum > 49 || userNum < 1) {
+    count = count + 1
+    if(count > 5){
+        alert("You can't follow instructions huh?");
+        break;
+    }
+    userNum = parseInt(prompt("Pick a odd number between 1 & 50"));
+}
+console.log("You picked the number " + userNum);
+if(userNum % 2 === 0 || isNaN(userNum) === true || userNum > 49 || userNum < 1){
+    console.log('User failed to provide correct number.');
+}else {
+    for (var i = 0; i <= 50; i++) {
+        if (i % 2 !== 0) {
+            if (i === userNum) {
+                console.log("Yikes, skipping the number " + userNum);
+                continue;
+            }
+            console.log(i);
+        }
+    }
+}
+
+
 // for (var i = 1; i < 50; i+=2) {
 // if (i === input) {
 //     console.log("yikes! skipping number : " + input);
@@ -15,18 +41,18 @@
 //         break;
 //     }
 // }
-var input = 0;
-
-while(input !== "youre never gonna come up with this"){
-   input = parseFloat(prompt("Choose a odd number from 1 top 50??"));
-    if (input % 2 === 1 || (input > 0 && input < 50)) {break;}
-}
-
-for (var i = 1; i < 50; i+=2) {
-    if (i === input) {
-        console.log("yikes! skipping number : " + input);
-        continue;
-    }
+// var input = 0;
+//
+// while(input !== "youre never gonna come up with this"){
+//    input = parseFloat(prompt("Choose a odd number from 1 top 50??"));
+//     if (input % 2 === 1 || (input > 0 && input < 50)) {break;}
+// }
+//
+// for (var i = 1; i < 50; i+=2) {
+//     if (i === input) {
+//         console.log("yikes! skipping number : " + input);
+//         continue;
+//     }
 }
 // for (var i = 1; i <=50; i++) {
 //     if (input % 2 !== 0) {
@@ -41,3 +67,4 @@ for (var i = 1; i < 50; i+=2) {
 //         alert("Please enter a valid number.");
 //     }
 // }
+
