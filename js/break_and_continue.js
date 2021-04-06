59ac1710bfc5c0c60109c98a767b73c91fa6a835
Juan Garcia <garcia.d.juan14@gmail.com>
2021-04-06 14:42:54 -0500

    fixing problems

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,66 +5,67 @@
 // var count = 0;
 // var userNum = parseInt(prompt("Pick a odd number between 1 & 50"));
 // while (userNum % 2 === 0 || isNaN(userNum) === true || userNum > 49 || userNum < 1) {
-//     count = count + 1
-//     if(count > 5){
-//         alert("You can't follow instructions huh?");
-//         break;
-//     }
-//     userNum = parseInt(prompt("Pick a odd number between 1 & 50"));
+//      count = count + 1
+//      if(count > 5){
+//          alert("You can't follow instructions huh?");
+//          break;
+//      }
+//      userNum = parseInt(prompt("Pick a odd number between 1 & 50"));
 // }
 // console.log("You picked the number " + userNum);
 // if(userNum % 2 === 0 || isNaN(userNum) === true || userNum > 49 || userNum < 1){
-//     console.log('User failed to provide correct number.');
+//      console.log('User failed to provide correct number.');
 // }else {
-//     for (var i = 0; i <= 50; i++) {
-//         if (i % 2 !== 0) {
-//             if (i === userNum) {
-//                 console.log("Yikes, skipping the number " + userNum);
-//                 continue;
-//             }
-//             console.log(i);
-//         }
-//     }
+//      for (var i = 0; i <= 50; i++) {
+//          if (i % 2 !== 0) {
+//              if (i === userNum) {
+//                  console.log("Yikes, skipping the number " + userNum);
+//                  continue;
+//              }
+//              console.log(i);
+//          }
+//      }
 // }
-
-
+//
+//
 // for (var i = 1; i < 50; i+=2) {
-// if (i === input) {
-//     console.log("yikes! skipping number : " + input);
-//     continue;
-//     var input = parseFloat(prompt("Choose a odd number from 1 top 50??"));
-//     if (input % 2 !== 1) {
-//         console.log("this number is not a odd number");
-//         break;
-//     }else if (input <1 && input > 50) {
-//         console.log( "this number does not fit in the asked prompt" );
-//         break;
-//     }
+//     if (i === input) {
+//          console.log("yikes! skipping number : " + input);
+//          continue;
+//          var input = parseFloat(prompt("Choose a odd number from 1 top 50??"));
+//          if (input % 2 !== 1) {
+//              console.log("this number is not a odd number");
+//              break;
+//          }else if (input <1 && input > 50) {
+//              console.log( "this number does not fit in the asked prompt" );
+//              break;
+//          }
+//      }
 // }
 // var input = 0;
 //
 // while(input !== "youre never gonna come up with this"){
-//    input = parseFloat(prompt("Choose a odd number from 1 top 50??"));
-//     if (input % 2 === 1 || (input > 0 && input < 50)) {break;}
+//     input = parseFloat(prompt("Choose a odd number from 1 top 50??"));
+//      if (input % 2 === 1 || (input > 0 && input < 50)) {break;}
 // }
 //
 // for (var i = 1; i < 50; i+=2) {
-//     if (i === input) {
-//         console.log("yikes! skipping number : " + input);
-//         continue;
-//     }
-}
+//      if (i === input) {
+//          console.log("yikes! skipping number : " + input);
+//          continue;
+// }
+// }
 // for (var i = 1; i <=50; i++) {
-//     if (input % 2 !== 0) {
-//         if (input > 0 && input < 50) {
-//             alert("That sure is an odd number between 1-50! \r\n\ Please check the console log for all further output.")
-//             break;
-//         } else {
-//             alert("Please enter a valid number.");
-//         }
+//      if (input % 2 !== 0) {
+//          if (input > 0 && input < 50) {
+//              alert("That sure is an odd number between 1-50! \r\n\ Please check the console log for all further output.")
+//              break;
+//          } else {
+//              alert("Please enter a valid number.");
+//          }
 //
-//     } else {
-//         alert("Please enter a valid number.");
-//     }
+//      } else {
+//          alert("Please enter a valid number.");
+//      }
 // }
 
