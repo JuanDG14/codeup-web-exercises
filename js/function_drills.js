8e8534a1fe46fb682debc2875356ea078e361bae
Juan Garcia <garcia.d.juan14@gmail.com>
2021-05-02 23:59:38 -0500

    little practice

--- js/function_drills.js
+++ js/function_drills.js
@@ -9,7 +9,7 @@
 // console.log(isOdd(100));
 // console.log(isOdd(12));
 // console.log(isOdd(13));
-//
+
 //
 // function isEven(input) {
 //     return (input % 2 === 0);
@@ -18,38 +18,73 @@
 // console.log(isEven(100));
 // console.log(isEven(12));
 // console.log(isEven(13));
-//
+
 // function identity(input) {
 //     return input;
 // }
+// console.log(identity("true"))
 // console.log(identity("hello my name is juan"));
 // console.log(identity(1));
 // console.log(identity(true));
 
-function isFive(input) {
-    return (input === 5);
-}
-console.log(isFive(5));
-console.log(isFive('five'));
-console.log(isFive("five"));
-console.log(isFive(true));
+// function isFive(input) {
+//     return (input === 5);
+// }
+// console.log(isFive(5));
+// console.log(isFive('five'));
+// console.log(isFive("five"));
+// console.log(isFive(true));
 
-function addFive(input) {
-    return (input + 5);
-}
-console.log(addFive(10));
-console.log(addFive('15'));
-console.log(addFive(true));
-console.log(addFive(NaN));
+// function addFive(input) {
+//     return (input + 5);
+// }
+// console.log(addFive(10));
+// console.log(addFive('15'));
+// console.log(addFive(true));
+// console.log(addFive(NaN));
 
-function isMultipleOfFive(input) {
-    return (input * 5);
-}
-console.log(isMultipleOfFive(130));
-console.log(isMultipleOfFive("50"));
-console.log(isMultipleOfFive(true));
-console.log(isMultipleOfFive(NaN));
+// function isMultipleOfFive(input) {
+//     if(input % 5 ===0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isMultipleOfFive(130));
+// console.log(isMultipleOfFive(45));
+// console.log(isMultipleOfFive(32));
+// console.log(isMultipleOfFive(30));
 
-function isThree(input) {
+// function isThree(input) {
+//     return input === 3
+// }
+// console.log(isThree(3))
+// console.log(isThree("3"))
+// console.log(isThree(true))
+// console.log(isThree(NaN))
 
-}
+// function isMultipleOfThree (input) {
+//     if(input % 3 ===0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isMultipleOfThree(130));
+// console.log(isMultipleOfThree(45));
+// console.log(isMultipleOfThree(32));
+// console.log(isMultipleOfThree(30));
+
+// function isMultipleOfThreeAndFive (input) {
+//     if (input % 3 === 0 && input % 5 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isMultipleOfThreeAndFive(45))
+// console.log(isMultipleOfThreeAndFive(130))
+// console.log(isMultipleOfThreeAndFive(15))
+// console.log(isMultipleOfThreeAndFive(300))
+// console.log(isMultipleOfThreeAndFive(99))
+
