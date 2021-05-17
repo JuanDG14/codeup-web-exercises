07fbc9920b1d99adf4ce9b5315522c8c803001fb
Juan Garcia <garcia.d.juan14@gmail.com>
2021-05-17 09:26:36 -0500

    pushing what i forgot to push on fridday

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,55 @@
+"use strict"
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+// old way .map
+let emails = users.map(function(user){
+    return user.email;
+})
+// es6 way .map
+let es6emails = users.map(user => user.email);
+
+
+
+//old way .filter
+let multilingual = users.filter(function (user){
+    return user.languages.length > 2;
+})
+// es6 .filter
+let result = users.filter(user => user.languages.length > 2);
+
