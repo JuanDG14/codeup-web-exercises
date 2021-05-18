d12c45119512a2e5e9ca2d87c3bea1dfcb5b1b30
Juan Garcia <garcia.d.juan14@gmail.com>
2021-05-18 09:10:43 -0500

    finished assignment

--- js/promise.js
+++ js/promise.js
@@ -2,7 +2,7 @@ function gitHubUsername (userName) {
     fetch (`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': PROMISE_TOKEN}})
         .then(response => response.json().then(data => {
         console.log(data);
-            var latestPush = data[0].created_at
+            let latestPush = data[0].created_at
             let date = new Date(latestPush);
 
             console.log(date.toString());
@@ -10,3 +10,8 @@ function gitHubUsername (userName) {
 }
 gitHubUsername('JuanDG14')
 
+function wait(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
