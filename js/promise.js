fc5c966b237ecb0bf5a84a4630db82cc25d89ba4
Juan Garcia <garcia.d.juan14@gmail.com>
2021-05-18 10:15:07 -0500

    revised work

--- js/promise.js
+++ js/promise.js
@@ -2,16 +2,33 @@ function gitHubUsername (userName) {
     fetch (`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': PROMISE_TOKEN}})
         .then(response => response.json().then(data => {
         console.log(data);
-            let latestPush = data[0].created_at
-            let date = new Date(latestPush);
-
-            console.log(date.toString());
+            // let latestPush = data[0].created_at
+            // let date = new Date(latestPush);
+            let lastPush;
+            for(let event of data) {
+                if(event.type === "PushEvent"){
+                    lastPush = new Date(event.created_at);
+                    break;
+                }
+            }
+                console.log(lastPush)
+            // console.log(date.toString());
     }))
 }
 gitHubUsername('JuanDG14')
 
-function wait(ms){
-    return new Promise(response => setTimeout(response, ms));
+// function wait(ms){
+//     return new Promise(response => setTimeout(response, ms));
+// }
+
+const wait = ms => {
+    return new Promise(response => {
+        setTimeout(() => {
+            response(`You'll see this after ${ms/1000} seconds`);
+        }, ms);
+    })
 }
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+wait(1000).then(message => console.log(message));
+wait(3000).then(message => console.log(message));
+wait(5000).then(message => console.log(message));
