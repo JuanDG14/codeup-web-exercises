e553d5b89dd11a5e0a95c9f8fb65a42fc58183d2
Juan Garcia <garcia.d.juan14@gmail.com>
2021-05-26 09:16:05 -0500

    daily warmup

--- js/warmups.js
+++ js/warmups.js
@@ -12,3 +12,13 @@ for (var i = 1; i <= 30; i++) {
 
 const return7 = () => 7;
 console.log(return7())
+
+function num (i){
+    if (typeof i == "number" ){
+        return (i * 5)
+    } else {
+        return 0
+    }
+}
+console.log(num(true))
+console.log(num(5))
