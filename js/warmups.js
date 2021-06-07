f5fef98c550d464009d22405419f7c6d123f133b
Juan Garcia <garcia.d.juan14@gmail.com>
2021-06-07 09:18:17 -0500

    Warm up

--- js/warmups.js
+++ js/warmups.js
@@ -22,3 +22,9 @@ function num (i){
 }
 console.log(num(true))
 console.log(num(5))
+
+// 1. the difference between .java file and .class file is that the .java file is what is typed and coded by the User and the .class is what is being compiled and run to the computer and being read and typed by the computer.
+
+// 2. The reason for so many data types for the precision of each one and we use them for the memory that each data type holds.
+
+// 3.JDK (Java Development Kit) contains all the required tools and libraries to develop and run Java applications, including JRE. JRE Java run time environment is a piece of software which contains JVM and other libraries required to execute Java programs.
